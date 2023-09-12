346ab65933e4e892f8e659bacf18c28db8819101
Guilherme Bertuol <82103102+guilherme141288@users.noreply.github.com>
2023-09-12 15:50:03 -0300

    Update SumTwo.py

--- SumTwo.py
+++ SumTwo.py
@@ -1,34 +1,30 @@
 
 
+
 from typing import List
 
-nums = [2,7,11,15]
-target = 9
-complement_dict = {}
+numbers = [2, 7, 11, 15]
+target_value = 9
+complement_indices = {}
 
 class Solution:
-    def twoSum(self, nums: List[int], target: int) -> List[int]:
-
-
-
-
-    # loop through the list of numbers
+    def findIndicesForTwoSum(self, nums: List[int], target: int) -> List[int]:
+        # Loop through the list of numbers
         for i, num in enumerate(nums):
-        # calculate the complement of the current number
+            # Calculate the complement of the current number
             complement = target - num
+            
+            # Check if the complement is already in the dictionary
+            if complement in complement_indices:
+                # If it is, return the indices of the two numbers
+                return [complement_indices[complement], i]
+            
+            # If the complement is not in the dictionary, add the current number and its index to the dictionary
+            complement_indices[num] = i
         
-        # check if the complement is already in the dictionary
-            if complement in complement_dict:
-            # if it is, return the indices of the two numbers
-                return [complement_dict[complement], i]
-        
-        # if the complement is not in the dictionary, add the current number and its index to the dictionary
-        complement_dict[num] = i
-    
-    # if no two numbers add up to the target, return an empty list
-        return [] 
+        # If no two numbers add up to the target, return an empty list
+        return []
 
-
-instance = Solution()
-print(instance.twoSum(nums, target))
+solver = Solution()
+print(solver.findIndicesForTwoSum(numbers, target_value))
 
